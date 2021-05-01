98461b998208cecda77a40a18fd2d1b039d79573
hiheehee <gkek41@naver.com>
2021-05-01 22:06:44 +0900

    2021/05/01 commit

--- BOJ/src/BOJ2234.java
+++ BOJ/src/BOJ2234.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -9,21 +9,23 @@ import java.util.StringTokenizer;
 public class BOJ2234 {
 	static int n;
 	static int m;
-	static int map[][];
-	static int roomCount = 0;
-	static int maxRoom = 0;
-	static int wallRoom = 0;
-	static HashMap<Integer, String> hm = new HashMap<>();
-	public static void main(String[] args) throws IOException{
+	static int map[][]; // 지도
+	static int roomCount = 0; // 이 성에 있는 방의 개수
+	static int maxRoom = 0; // 	가장 넓은 방의 넓이
+	static int wallRoom = 0; // 하나의 벽을 제거하여 얻을 수 있는 가장 넓은 방의 크기
+	static int dx[] = {0,-1,0,1}; // 서 북 동 남
+	static int dy[] = {-1,0,1,0}; // 서 북 동 남
+	static boolean visited[][];
+	static int wallCount[][][];
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		map = new int[m][n];
-		hm.put(1, "0 -1"); // 서쪽
-		hm.put(2, "-1 0"); // 북쪽
-		hm.put(4, "0 1"); // 동쪽
-		hm.put(8, "1 0"); // 남쪽
+		visited = new boolean[m][n];
+		wallCount = new int[m][n][2];
 		
 		for(int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -32,63 +34,87 @@ public class BOJ2234 {
 			}
 		}
 
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				if(!visited[i][j]) {
+					bfs(i, j, roomCount);
+					roomCount++;
+				}
+			}
+		}
+		
+		MaxRoom();
+
+		System.out.println(roomCount);
+		System.out.println(maxRoom);
+		System.out.println(wallRoom);
 	}
 	
-	static void bfs(int x, int y) {
+	static void bfs(int x, int y, int id) {
 		Queue<coordiante> q = new LinkedList<>();
-		boolean visited[][][] = new boolean[m][n][2];
-		q.offer(new coordiante(x, y, 0, 0));
+		q.offer(new coordiante(x, y));
 		int tempRoom = 0;
-		int tempWallRoom = 0;
+		ArrayList<coordiante> back = new ArrayList<>();
+		visited[x][y] = true;
 		
 		while(!q.isEmpty()) {
 			coordiante cur = q.poll();
+			tempRoom++;
+			back.add(new coordiante(cur.x, cur.y));
 			
 			for(int i = 0; i < 4; i++) {
-				String temp[] = hm.get((int)Math.pow(2, i)).split(" ");
-				int tx = cur.x + Integer.parseInt(temp[0]);
-				int ty = cur.y + Integer.parseInt(temp[1]);
+				int tx = cur.x + dx[i];
+				int ty = cur.y + dy[i];
 				
-				if((map[cur.x][cur.y] & (1<<i)) == 0) { // 벽이 없는 경우				
+				if((map[cur.x][cur.y] & (1 << i)) == 0) { // 벽이 없는 경우				
 					if(tx < 0 || m <= tx || ty < 0 || n <= ty) {
 						continue;
 					}
 					
-					if(visited[tx][ty][cur.count]) {
-						continue;
-					}
-					tempRoom++;
-					tempWallRoom++;
-					q.offer(new coordiante(tx, ty, cur.count+1, cur.wall));
-				}else if((map[cur.x][cur.y] & (1<<i)) == 1 && cur.wall == 0) {
-					if(tx < 0 || m <= tx || ty < 0 || n <= ty) {
+					if(visited[tx][ty]) {
 						continue;
 					}
 					
-					if(visited[tx][ty][cur.wall+1]) {
-						continue;
-					}
-					tempWallRoom++;
-					q.offer(new coordiante(tx, ty, cur.count+1, cur.wall+1));
+					visited[tx][ty] = true;
+					q.offer(new coordiante(tx, ty));
 				}
 			}
 		}
 		
 		maxRoom = Math.max(maxRoom, tempRoom);
-		wallRoom = Math.max(wallRoom, tempWallRoom);
+
+		for(coordiante c: back) {
+			wallCount[c.x][c.y][1] = tempRoom;
+			wallCount[c.x][c.y][0] = id;
+		}
+	}
+	
+	static void MaxRoom() {
+		for(int i = 0; i < m; i++) {
+			for(int j = 0; j < n; j++) {
+				for(int k = 0; k < 4; k++) {
+					int tx = i + dx[k];
+					int ty = j + dy[k];
+					
+					if(tx < 0 || m <= tx || ty < 0 || n <= ty) {
+						continue;
+					}
+					
+					if(wallCount[i][j][0] != wallCount[tx][ty][0]) {
+						wallRoom = Math.max(wallRoom, wallCount[i][j][1]+wallCount[tx][ty][1]);
+					}
+				}
+			}
+		}
 	}
 	
 	static class coordiante {
 		int x;
 		int y;
-		int count;
-		int wall;
 		
-		coordiante(int x, int y, int count, int wall) {
+		coordiante(int x, int y) {
 			this.x = x;
 			this.y = y;
-			this.count = count;
-			this.wall = wall;
 		}
 	}
 
