2727ad99d160db7540be6b1f887cc8cd0d177856
hiheehee <gkek41@naver.com>
2021-05-30 00:29:07 +0900

    2021/05/30 commit

--- BOJ/src/BOJ15685.java
+++ BOJ/src/BOJ15685.java
@@ -2,8 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class BOJ15685 {
 	static boolean visited[][] = new boolean[101][101];
